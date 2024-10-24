b1f0664aa6875a852124f604b3fc01819e11a71e
raj-612 <rajkumar.mannam@aestheticrecord.com>
2024-10-24 14:43:53 -0500

    lead ar chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Authenticator} from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import outputs from "../amplify_outputs.json";
 import OrderingDashboard from './components/OrderingDashboard';
-import ChatWidget from './components/chatWidget';
+import { useEffect } from 'react';
 
 Amplify.configure(outputs);
 
 export default function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://widgets.leadconnectorhq.com/loader.js";
+    script.setAttribute('data-resources-url', 'https://widgets.leadconnectorhq.com/chat-widget/loader.js');
+    script.setAttribute('data-widget-id', '671a9d069d106a9e690febe9');
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Authenticator className='flex justify-center items-center h-screen'>
       {({ signOut, user }) => (
@@ -22,7 +35,7 @@ export default function App() {
           </div>
           <main className="flex flex-1">
             <OrderingDashboard user={user} />
-            <ChatWidget />
+            {/* <ChatWidget /> */}
           </main>
         </div>
       )}
